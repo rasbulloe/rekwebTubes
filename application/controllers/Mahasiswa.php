bea44ccd2a444b99dcccd71ee9fd5033d0cba8c8
Dede Aminudin <rasbulloe@gmail.com>
2019-10-20 22:14:31 +0700

    menambahkan header dan fungsi headernya

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -1,10 +1,20 @@
 <?php 
 
     class Mahasiswa extends CI_Controller {
+
+        public function __construct() {
+            parent::__construct();
+            $this->load->model('Mahasiswa_model');
+
+        }
        
         public function index() {
 
-            $this->load->model('Mahasiswa_model');
+            $data['judul'] = 'Daftar Mahasiswa';
+
+            $this->load->view('templates/header', $data);
+            $this->load->view('templates/footer');
+
 
             $data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
             $this->load->view('mahasiswa/index', $data);
