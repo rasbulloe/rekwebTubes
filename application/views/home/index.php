80b0639989f14345fc8baac8c5cb512b2ec75c55
Erdika Rhamadan K <erdikar.kusumah@mail.unpas.ac.id>
2019-12-08 23:18:04 +0700

    menambahkan asset fontawesome, img. membuat navbar dan tampilan home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,2 +1,21 @@
-    <h1>Hello, <?= $nama; ?> </h1>
-
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+              <div class="carousel-item active">
+                  <img src="<?= base_url(); ?>/assets/img/home/slide1.jpg" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                  <img src="<?= base_url(); ?>/assets/img/home/slide2.jpg" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                  <img src="<?= base_url(); ?>/assets/img/home/slide3.jpg" class="d-block w-100" alt="...">
+              </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+          </a>
+      </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,35 +1,64 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css"> -->
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title> <?= $judul ?> </title>
-  </head>
-  <body>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <!-- <link rel="stylesheet" href="<?= base_url(); ?>assets/css/style.css"> -->
+  <link rel="stylesheet" href="<?= base_url(); ?>/assets/fontawesome/css/all.min.css">
 
-  <nav class="navbar navbar-expand-lg navbar-primary bg-warning">
-  <div class="container ml-5">
-  <a class="navbar-brand" href="#">I-KAE</a>
-  </div>
-  <div class="container">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link" href="<?= base_url(); ?>">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="<?= base_url(); ?>">Kategori</a>
-      <a class="nav-item nav-link" href="<?= base_url(); ?>">About</a>
-    <div class="contaniner">
-      <a class="nav-item nav-link" href="<?= base_url('auth'); ?>">Login</a>
+  <title> <?= $judul ?> </title>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
+    <div class="container" ml-5>
+      <a class="navbar-brand font-weight-bold" href="#"><img src="<?= base_url(); ?>/assets/img/logo.png" height="50px" weight="50px" alt="">
+        Astro.
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto mr-4">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Shop
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Detai Product</a>
+              <a class="dropdown-item" href="#">Shop Category</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Help</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <div class="icon mt-2">
+          <h5>
+            <a href="#">
+              <i class="far fa-heart ml-3 mr-3 text-dark"></i>
+            </a>
+            <a href="#">
+              <i class="fas fa-cart-plus text-dark"></i>
+            </a>
+          </h5>
+        </div>
+      </div>
     </div>
-  </div>
-  </div>
-</div>
-</nav>
+  </nav>
