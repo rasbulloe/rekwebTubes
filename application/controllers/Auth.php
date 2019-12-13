d1377cf96969cc0c7dad7491ba4c36cec7829d32
rasbulloe <rasbulloe.com>
2019-12-13 08:39:31 +0700

    menambahkan method register

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,6 +3,11 @@ defined('BASEPATH') or exit('No direct script aceess allowed');
 
 class Auth extends CI_Controller {
 
+    public function __construct() {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
     public function index() {
 
         $this->load->view('templates/auth_header');
@@ -13,11 +18,23 @@ class Auth extends CI_Controller {
 
     public function registration() {
 
-        $this->load->view('templates/auth_header');
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email');
+
+        if($this->form_validation->run() == false) {
+        $data['title'] = 'User Registration';
+        $this->load->view('templates/auth_header', $data);
         $this->load->view('auth/registration');
         $this->load->view('templates/auth_header');
-
+        } else {
+            echo 'data berhasil ditambahkan!';
+        }
     }
+
+
+
+
+
 
 }
 
