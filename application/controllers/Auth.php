df3610e6554ee4a26cdc453df20c33dfdabb17c0
rasbulloe <rasbulloe@gmail.com>
2019-12-14 12:53:16 +0700

    menambahkan fungsi logout

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -93,4 +93,14 @@ class Auth extends CI_Controller
             redirect('auth');
         }
     }
+
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">You have been logged out!</div>');
+        redirect('home');
+    }
 }
--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,2 +1,17 @@
 <?php
 defined('BASEPATH') or exit('No direct script aceess allowed');
+
+
+class User extends CI_Controller
+{
+
+    public function index()
+    {
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $data['judul'] = 'Astro Shop';
+        $this->load->view('templates/header', $data);
+        $this->load->view('user/index');
+        $this->load->view('templates/footer');
+    }
+}
