76b858461075c706483d45d84e5470c4c97982a0
Erdika Rhamadan K <erdikar.kusumah@mail.unpas.ac.id>
2019-12-20 17:21:45 +0700

    link logo

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -24,7 +24,7 @@
 
   <nav class="navbar navbar-expand-lg navbar-light bg-warning fixed-top">
     <div class="container" ml-5>
-      <a class="navbar-brand font-weight-bold" href="#"><img src="<?= base_url(); ?>/assets/img/logo.png" height="90px" weight="90px" alt="">
+      <a class="navbar-brand font-weight-bold" href="<?= base_url(); ?>"><img src="<?= base_url(); ?>/assets/img/logo.png" height="90px" weight="90px" alt="">
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
