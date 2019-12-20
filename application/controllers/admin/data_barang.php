3814c5c2a4fc2be90399f7b5777b608af3129b9f
Indrawansyah Prasetyo <indrawnsyhprstyo@gmail.com>
2019-12-20 16:52:48 +0700

    Memperbaiki uploud gambar pada tambah produk

--- application/controllers/admin/data_barang.php
+++ application/controllers/admin/data_barang.php
@@ -12,35 +12,54 @@ class Data_barang extends CI_Controller
 
     public function tambah_aksi()
     {
-
         $nama_brg   = $this->input->post('nama_brg');
         $keterangan_short = $this->input->post('keterangan_short');
         $kategori   = $this->input->post('kategori');
         $harga      = $this->input->post('harga');
         $stok       = $this->input->post('stok');
         $gambar     = $_FILES['gambar']['name'];
-        if ($gambar = '') {
-        } else {
+        // if ($gambar = '') {'
+            
+            
+        //     '
+        // } else {
+        //     $config['upload_path'] = './assets/img/home/';
+        //     $config['allowed_types'] = 'jpg|jpeg|png|gif';
+
+        //     $this->load->library('upload', $config);
+
+        //     if (!$this->upload->do_upload('gambar')) {
+        //         echo "Gambar Gagal diupload!";
+        //     } else {
+        //         $gambar = $this->upload->data('file_name');
+        //     }
+        // }
+
+        if ($gambar) {
+            $config['allowed_types'] = 'gif|jpg|jpeg|png';
             $config['upload_path'] = './assets/img/home/';
-            $config['allowed_types'] = 'jpg|jpeg|png|gif';
+            $config['file_name'] = $_FILES['gambar']['name'];
 
             $this->load->library('upload', $config);
-            if (!$this->upload->do_upload('gambar')) {
-                echo "Gambar Gagal diupload!";
+            // $this->upload->initialize($config);
+
+            if ($this->upload->do_upload('gambar')) {
+               $upload_image = $this->upload->data('file_name');
+               $this->db->set('gambar', $upload_image);
             } else {
-                $gambar = $this->upload->data('file_name');
+               redirect('admin/data_barang');
             }
-        }
+         }
 
-        $data = array(
-            'nama_brg'      => $nama_brg,
+         $data = array(
+            'nama_brg'          => $nama_brg,
             'keterangan_short'    => $keterangan_short,
-            'kategori'      => $kategori,
-            'harga'         => $harga,
-            'stok'          => $stok,
-            'gambar'        => $gambar
+            'kategori'          => $kategori,
+            'harga'             => $harga,
+            'stok'              => $stok,
+            'gambar' => $gambar,
         );
-
+        
         $this->Barang_model->tambah_barang($data, 'tb_barang');
         redirect('admin/data_barang/index');
     }
