f7e06026639d6e5952874cb0e5494dca5c4d108a
Erdika Rhamadan K <erdikar.kusumah@mail.unpas.ac.id>
2019-12-26 21:05:58 +0700

    merubah link tambahkekeranjang di detai_barang.php

--- application/views/detail_barang.php
+++ application/views/detail_barang.php
@@ -35,7 +35,7 @@
                     </tr>
                 </table>
 
-                <?php echo anchor('user/tambah_ke_keranjang/' . $brg->id_brg, '<div class="btn btn-success mt-3">Tambah ke Keranjang</div>') ?>
+                <div class="btn btn-success mt-3"><a href="<?= base_url('auth')?>" class="text-white">Tambah ke Keranjang</a></div>
 
                 <?php echo anchor('user/index/', '<div class="btn btn-danger mt-3">Kembali</div>') ?>
                 </div>
