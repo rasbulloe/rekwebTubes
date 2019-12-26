9c0c8597cb7e2ee7d5e9fa4eaea399a9f1a1118c
Erdika Rhamadan K <erdikar.kusumah@mail.unpas.ac.id>
2019-12-26 21:56:09 +0700

    tampil detail

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -47,7 +47,7 @@ class Home extends CI_Controller
     //     );
 
     //     $this->cart->insert($data);
-    //     $this->session->set_flashdata('flash', 'Ditambahkan');
+    //     // $this->session->set_flashdata('flash', 'Ditambahkan');
     //     redirect('home');
     // }
 
@@ -89,11 +89,11 @@ class Home extends CI_Controller
     //     }
     // }
 
-    // public function detail($id_brg)
-    // {
-    //     $data['barang'] = $this->Barang_model->detail_brg($id_brg);
-    //     $this->load->view('templates/header');
-    //     $this->load->view('detail_barang',$data);
-    //     $this->load->view('templates/footer');
-    // }
+    public function detail($id_brg)
+    {
+        $data['barang'] = $this->Barang_model->detail_brg($id_brg);
+        $this->load->view('templates/header');
+        $this->load->view('detail_barang',$data);
+        $this->load->view('templates/footer');
+    }
 }
